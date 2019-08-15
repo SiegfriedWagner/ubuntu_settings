43da2d82a763f1914bd4c6d5fb3805c712ed04d5
SiegfriedWagner <mateus.chojnowski@gmail.com>
2019-08-15 11:37:50 +0200

    added guake, fixed antigen

--- .custom.sh
+++ .custom.sh
@@ -1,1 +1,3 @@
-sudo apt install -y zsh-theme-powerlevel9k zsh-common zsh-antigen htop tree code
+sudo apt install -y zsh-theme-powerlevel9k zsh-common htop tree code guake
+# ubuntu repository antigen is broken and installation with curl is required
+sudo curl -o /usr/share/zsh-antigen/antigen.zsh -sL git.io/antigen
